118411a29373320bad5355ec01293616a6c2bc59
Kaki <perrinolivier88@gmail.com>
2016-11-03 13:38:01 +0100

    contact form completed

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,15 +10,18 @@ class ContactController extends AppController
     public function index()
     {
         if ($this->request->is('post')) {
-            $data = [$this->request->data['name'], $this->request->data['phone']];
-
+            $recipients = ['perrinolivier88@gmail.com','florent.maillard.pro@gmail.com','prevotgwenael@gmail.com',
+            'audrey.toussaints@gmail.com', 'znirgal@gmail.com', 'dassigny.kevin@hotmail.fr', 'alexis.nazimek88@gmail.com'];
+            $data = [$this->request->data['name'], $this->request->data['email'], $this->request->data['phone']];
+foreach ($recipients as $recipient){
             $email = new Email('default');
             $email->viewVars(['data' => $data]);
             $email->template('form', 'default')
                 ->emailFormat('html');
-            $email->to('perrinolivier88@gmail.com')
+            $email->to($recipient)
                 ->subject('Vous avez reçu une demande via le formulaire de contact')
                 ->send($this->request->data['message']);
+}
 }
     }
 
